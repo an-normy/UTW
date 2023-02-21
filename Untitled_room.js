d84676787987a1000d950d2b90af116484cbd6e3
an-normy <112652463+an-normy@users.noreply.github.com>
2023-02-21 17:58:01 -0300

    Delete Untitled_room.js

--- Untitled_room.js
+++
@@ -1,9 +0,0 @@
-function Logout() {
-    localStorage.removeItem("SU")
-    window.location = "index.html"
-}
-
-function addRoom() {
-    var adrom = document.getElementById("rooms").value
-    localStorage.setItem("ar", adrom)
-}
